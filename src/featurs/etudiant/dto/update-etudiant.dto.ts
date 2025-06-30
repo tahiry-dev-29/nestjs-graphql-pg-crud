db61aebd0c112ade03b33d44f340cf4aa8210e1f
Tanner tahiry <tahirydev29@gmail.com>
2025-06-30 23:31:26 +0300

    refactor: remove all files related to the NestJS application structure and functionality

--- src/featurs/etudiant/dto/update-etudiant.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEtudiantDto } from './create-etudiant.dto';
-
-export class UpdateEtudiantDto extends PartialType(CreateEtudiantDto) {}
--- src/featurs/etudiant/etudiant.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EtudiantService } from './etudiant.service';
-import { EtudiantController } from './etudiant.controller';
-
-@Module({
-  controllers: [EtudiantController],
-  providers: [EtudiantService],
-})
-export class EtudiantModule {}
